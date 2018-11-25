eeceaa3598e483147b599c767bc805ca6d8b0f59
Anthony <huasipango@gmail.com>
2018-11-25 10:07:08 -0500

    agregado plantilla y metidos divs con juego

--- script.js
+++ script.js
@@ -139,3 +139,69 @@ function generateQA(){
         }
     }
 }
+
+
+
+
+// SCRIPTS FROM TEMPLATE-------------------------------------------------------------
+
+var Expand = (function() {
+  var tile = $('.strips__strip');
+  var tileLink = $('.strips__strip > .strip__content');
+  var tileText = tileLink.find('.strip__inner-text');
+  var stripClose = $('.strip__close');
+  
+  var expanded  = false;
+
+  var open = function() {
+      
+    var tile = $(this).parent();
+
+      if (!expanded) {
+        tile.addClass('strips__strip--expanded');
+        // add delay to inner text
+        tileText.css('transition', 'all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)');
+        stripClose.addClass('strip__close--show');
+        stripClose.css('transition', 'all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)');
+        expanded = true;
+      } 
+    };
+  
+  var close = function() {
+    if (expanded) {
+      tile.removeClass('strips__strip--expanded');
+      // remove delay from inner text
+      tileText.css('transition', 'all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)');
+      stripClose.removeClass('strip__close--show');
+      stripClose.css('transition', 'all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)')
+      expanded = false;
+    }
+  }
+
+    var bindActions = function() {
+      tileLink.on('click', open);
+      stripClose.on('click', close);
+    };
+
+    var init = function() {
+      bindActions();
+    };
+
+    return {
+      init: init
+    };
+
+  }());
+
+Expand.init();
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
